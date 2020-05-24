f63ed6960ef697c78a93da6d97bb0c6a43f2cd1f
Andrey <Sibir_91@list.ru>
2020-05-25 02:58:15 +0400

    final

---
+++ src/js/menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.hamburger').click(function(event) {
+        $('.hamburger').toggleClass('hamburger_active');
+    });
+});
+
---
+++ src/js/menu_list.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.hamburger').click(function(event) {
+        $('.menu').toggleClass('menu_active');
+    });
+});
